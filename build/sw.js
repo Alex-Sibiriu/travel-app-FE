0d9f08de79fe20ce3eb2cd16846ba42ab86f635c
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-08-26 14:06:17 +0200

    [add] build

---
+++ build/sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "appV1";
+
+// Questo array include le risorse critiche da precacheare
+const urlsToCache = [
+	"/",
+	"/auth",
+	"/offline",
+	"/index.html",
+	"/static/js/bundle.js",
+	"/static/js/main.chunk.js",
+	"/static/js/0.chunk.js",
+];
+
+this.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+this.addEventListener("fetch", (event) => {
+	// console.log("Fetching:", event.request.url);
+
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (response) {
+				// console.log("Serving from cache:", event.request.url);
+				return response;
+			}
+
+			return fetch(event.request)
+				.then((networkResponse) => {
+					if (
+						!networkResponse ||
+						networkResponse.status !== 200 ||
+						networkResponse.type !== "basic"
+					) {
+						return networkResponse;
+					}
+
+					const responseToCache = networkResponse.clone();
+					caches.open(CACHE_NAME).then((cache) => {
+						// console.log("Caching new resource:", event.request.url);
+						cache.put(event.request, responseToCache);
+					});
+
+					return networkResponse;
+				})
+				.catch((error) => {
+					console.error("Fetch failed:", error);
+				});
+		})
+	);
+});
+
+this.addEventListener("activate", (event) => {
+	const cacheWhitelist = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+});
