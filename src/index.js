2c2eb95a38a15825b52ecb71e7fa6802ec9fa5ef
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-08-27 15:35:39 +0200

    build

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store";
-import swDev from "./swDev";
+// import swDev from "./swDev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,4 +16,24 @@ root.render(
 	</React.StrictMode>
 );
 
-swDev();
+// Funzione per registrare il service worker
+function registerServiceWorker() {
+	if ("serviceWorker" in navigator) {
+		window.addEventListener("load", () => {
+			const swPath = "/sw.js";
+			navigator.serviceWorker
+				.register(swPath)
+				.then((registration) => {
+					console.log(
+						"ServiceWorker registration successful with scope: ",
+						registration.scope
+					);
+				})
+				.catch((error) => {
+					console.error("ServiceWorker registration failed: ", error);
+				});
+		});
+	}
+}
+
+registerServiceWorker();
